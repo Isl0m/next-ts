89d211bd75d68f2e6e155193b7f3669213d3610c
isl0m <islomrustam09@gmail.com>
2022-05-09 08:33:06 +0500

    changing structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
+import Layout from '../app/components/Layout';
+import '../styles/globals.css';
 import '../firebase';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   );
 }
